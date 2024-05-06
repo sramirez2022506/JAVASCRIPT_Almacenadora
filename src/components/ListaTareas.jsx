e3bac337fbbf335c4f6b57586461e95f316ae9c4
cjuarez2017159 <cjuarez-2017159@kinal.edu.gt>
2024-05-06 16:44:16 -0600

    Css arreglado

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
-import './ListaTareas.css';
+import "./ListaTareas.css";
+import icono from "../assets/Img/icon-1.png";
 
 export const FormPage = () => {
   const [tasks, setTasks] = useState([]);
-  const [taskName, setTaskName] = useState('');
-  const [description, setDescription] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
+  const [taskName, setTaskName] = useState("");
+  const [description, setDescription] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
   const [incomplete, setIncomplete] = useState(false);
-  const [creatorName, setCreatorName] = useState('');
-  const [creatorLastName, setCreatorLastName] = useState('');
+  const [creatorName, setCreatorName] = useState("");
+  const [creatorLastName, setCreatorLastName] = useState("");
   const [editingTask, setEditingTask] = useState(null);
   const [editingIndex, setEditingIndex] = useState(null);
 
   const handleAddTask = (e) => {
     e.preventDefault();
-    if (taskName.trim() !== '') {
+    if (taskName.trim() !== "") {
       const newTask = {
         id: Date.now(),
         taskName: taskName,
@@ -24,7 +25,7 @@ export const FormPage = () => {
         endDate: endDate,
         incomplete: incomplete,
         creatorName: creatorName,
-        creatorLastName: creatorLastName
+        creatorLastName: creatorLastName,
       };
       if (editingTask !== null) {
         const updatedTasks = [...tasks];
@@ -35,25 +36,25 @@ export const FormPage = () => {
       } else {
         setTasks([...tasks, newTask]);
       }
-      setTaskName('');
-      setDescription('');
-      setStartDate('');
-      setEndDate('');
+      setTaskName("");
+      setDescription("");
+      setStartDate("");
+      setEndDate("");
       setIncomplete(false);
-      setCreatorName('');
-      setCreatorLastName('');
+      setCreatorName("");
+      setCreatorLastName("");
     }
   };
 
   const handleDeleteTask = (taskId) => {
-    setTasks(tasks.filter(task => task.id !== taskId));
+    setTasks(tasks.filter((task) => task.id !== taskId));
   };
 
   const handleEditTask = (taskId) => {
-    const taskToEdit = tasks.find(task => task.id === taskId);
+    const taskToEdit = tasks.find((task) => task.id === taskId);
     if (taskToEdit) {
       setEditingTask(taskToEdit);
-      setEditingIndex(tasks.findIndex(task => task.id === taskId));
+      setEditingIndex(tasks.findIndex((task) => task.id === taskId));
       setTaskName(taskToEdit.taskName);
       setDescription(taskToEdit.description);
       setStartDate(taskToEdit.startDate);
@@ -63,46 +64,89 @@ export const FormPage = () => {
       setCreatorLastName(taskToEdit.creatorLastName);
     }
   };
- 
+
   return (
     <div className="app-container">
-      <h1>To Do List</h1>
+      <h1 className="title">
+        <img className="icon" src={icono} alt="icon" />
+        <span className="vertical-center">ToDo List</span>
+      </h1>
       <form className="container-form-1" onSubmit={handleAddTask}>
-      <li>
-        <div>
-          <label>Task Name</label>
-          <input type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
-        </div>
-        <div>
-          <label>Description</label>
-          <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
-        </div>
-        <div>
-          <label>Start Date</label>
-          <input type="text" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
-        </div>
-        <div>
-          <label>End Date</label>
-          <input type="text" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
-        </div>
-        <div>
-          <label>Creator's Name</label>
-          <input type="text" value={creatorName} onChange={(e) => setCreatorName(e.target.value)} />
-        </div>
-        <div>
-          <label>Creator's Last Name</label>
-          <input type="text" value={creatorLastName} onChange={(e) => setCreatorLastName(e.target.value)} />
-        </div>
-        <div>
-          <label>Incomplete</label>
-          <input type="checkbox" checked={incomplete} onChange={(e) => setIncomplete(e.target.checked)} />
-        </div>
-        <button className="add-button" type='submit'>{editingTask ? 'Update Task' : 'Add Task'}</button>
+        <li className="list-1">
+          <div>
+            <label>Task Name</label>
+            <input
+              type="text"
+              value={taskName}
+              onChange={(e) => setTaskName(e.target.value)}
+              required="true"
+            />
+          </div>
+          <div>
+            <label>Description</label>
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required="true"
+            />
+          </div>
+          <div>
+            <label>Start Date</label>
+            <br></br>
+            <input
+              className="dates"
+              type="date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+              required="true"
+            />
+          </div>
+          <div>
+            <label>End Date</label>
+            <br></br>
+            <input
+              className="dates"
+              type="date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+              required="true"
+            />
+          </div>
+          <div>
+            <label>Creators Name</label>
+            <input
+              type="text"
+              value={creatorName}
+              onChange={(e) => setCreatorName(e.target.value)}
+              required="true"
+            />
+          </div>
+          <div>
+            <label>Creators Last Name</label>
+            <input
+              type="text"
+              value={creatorLastName}
+              onChange={(e) => setCreatorLastName(e.target.value)}
+              required="true"
+            />
+          </div>
+          <div>
+            <label>Incomplete</label>
+            <input
+              type="checkbox"
+              checked={incomplete}
+              onChange={(e) => setIncomplete(e.target.checked)}
+            />
+          </div>
+          <button className="add-button" type="submit">
+            {editingTask ? "Update Task" : "Add Task"}
+          </button>
         </li>
       </form>
       <ul className="container-form-2">
-        {tasks.map(task => (
-          <li key={task.id}>
+        {tasks.map((task) => (
+          <li className="list-2" key={task.id}>
             <div>
               <strong>Task Name:</strong> {task.taskName}
             </div>
@@ -116,20 +160,32 @@ export const FormPage = () => {
               <strong>End Date:</strong> {task.endDate}
             </div>
             <div>
-              <strong>Incomplete:</strong> {task.incomplete ? 'Yes' : 'No'}
+              <strong>Incomplete:</strong> {task.incomplete ? "Yes" : "No"}
             </div>
             <div>
-              <strong>Creator's Name:</strong> {task.creatorName}
+              <strong>Creators Name:</strong> {task.creatorName}
             </div>
             <div>
-              <strong>Creator's Last Name:</strong> {task.creatorLastName}
+              <strong>Creators Last Name:</strong> {task.creatorLastName}
             </div>
-            <button className="edit-button" onClick={() => handleEditTask(task.id)}>Edit Task</button>
-            <button className="delete-button" onClick={() => handleDeleteTask(task.id)}>Delete Task</button>
+            <button
+              className="edit-button"
+              onClick={() => handleEditTask(task.id)}
+            >
+              Edit Task
+            </button>
+            <button
+              className="delete-button"
+              onClick={() => handleDeleteTask(task.id)}
+            >
+              Delete Task
+            </button>
           </li>
         ))}
       </ul>
+      <br></br>
     </div>
+    
   );
 };
 
